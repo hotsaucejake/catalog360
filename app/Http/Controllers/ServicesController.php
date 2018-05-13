f88be2925e4081535b5f518939129e9cb3858660
hotsaucejake <get@hotsaucejake.com>
2018-05-13 21:45:17 +0000

    atg and transform360 services api
    - Reason(s):
    
    - Change(s):
    
    - Reference(s):

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -6,31 +6,63 @@ use Illuminate\Http\Request;
 
 class ServicesController extends Controller
 {
-   /**
-   * Create a new controller instance.
-   *
-   * @return void
-   */
-   public function __construct() {}
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {}
 
-   /**
-   * Show the application dashboard.
-   *
-   * @return \Illuminate\Http\Response
-   */
-   public function index()
-   {
-      $services = \App\Service::all();
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $services = \App\Service::all();
 
-      $services->map(function ($service) {
-         $service['vendor'] = $service->vendor;
-      });
-      
-      $services->map(function ($service) {
-         $service->image = 'https://catalog.corus360.com/storage/' . $service->image;
-      });
+        $services->map(function ($service) {
+            $service['vendor'] = $service->vendor;
+        });
+        
+        $services->map(function ($service) {
+            $service->image = 'https://catalog.corus360.com/storage/' . $service->image;
+        });
 
 
-      return response()->json($services);
-   }
+        return response()->json($services);
+    }
+
+
+    public function atg()
+    {
+        $services = \App\Service::where('category_id', 1)->get();
+
+        $services->map(function ($service) {
+            $service['vendor'] = $service->vendor;
+        });
+        
+        $services->map(function ($service) {
+            $service->image = 'https://catalog.corus360.com/storage/' . $service->image;
+        });
+
+        return response()->json($services);
+    }
+
+
+    public function transform360()
+    {
+        $services = \App\Service::where('category_id', 2)->get();
+
+        $services->map(function ($service) {
+            $service['vendor'] = $service->vendor;
+        });
+        
+        $services->map(function ($service) {
+            $service->image = 'https://catalog.corus360.com/storage/' . $service->image;
+        });
+
+        return response()->json($services);
+    }
 }
